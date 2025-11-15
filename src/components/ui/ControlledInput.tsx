2ee19c2250eb1c83a2b5757125926985766a7deb
mtslma <mateusdasilveiralima@gmail.com>
2025-11-15 00:44:12 -0300

    alterações nesse componente sendo a principal delas resolvendo o erro de chamar as cores do medix, antiga configuração do tailwind

--- src/components/ui/ControlledInput.tsx
+++ src/components/ui/ControlledInput.tsx
@@ -1,11 +1,11 @@
 import inputControllerProps from "@/types/ui/controlledInputTypes";
-import { Controller, FieldValues } from "react-hook-form";
+import { Controller, FieldValues, Path } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 
 export function ControlledInput<T extends FieldValues>({ control, name, label, error, formatValue, ...rest }: inputControllerProps<T>) {
     return (
         <View>
-            <Text className="mb-1 text-base font-semibold text-medix-700">{label}</Text>
+            <Text className="mb-1 text-base font-semibold text-neutral-700">{label}</Text>
 
             <Controller
                 name={name}
@@ -16,11 +16,21 @@ export function ControlledInput<T extends FieldValues>({ control, name, label, e
                         onChange(formattedValue);
                     };
 
-                    return <TextInput className={`bg-[#FAF9F6] border ${error ? "border-medix-error" : "border-medix-300"} rounded-lg p-3 text-base text-medix-900`} onBlur={onBlur} onChangeText={handleChange} value={value} {...rest} />;
+                    return (
+                        <TextInput
+                            className={`bg-[#FAF9F6] border 
+                            ${error ? "border-onematter-700" : "border-neutral-300"} 
+                            rounded-lg p-3 text-base text-neutral-900`}
+                            onBlur={onBlur}
+                            onChangeText={handleChange}
+                            value={formatValue && value ? formatValue(value) : value}
+                            {...rest}
+                        />
+                    );
                 }}
             />
 
-            {error && <Text className="mt-1 text-sm text-medix-error">{error.message}</Text>}
+            {error && <Text className="mt-1 text-sm text-onematter-700">{error.message}</Text>}
         </View>
     );
 }
