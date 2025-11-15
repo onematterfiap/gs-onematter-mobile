ffe55738855869124960b984112544f5743c6bae
mtslma <mateusdasilveiralima@gmail.com>
2025-11-15 00:36:33 -0300

    adicionado uma tela de not found, depois tenho que trabalhar um pouco mais nela

---
+++ src/app/+not-found.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+const NotFound = () => {
+    return (
+        <View>
+            <Text>NotFound</Text>
+        </View>
+    );
+};
+export default NotFound;
+const styles = StyleSheet.create({});
